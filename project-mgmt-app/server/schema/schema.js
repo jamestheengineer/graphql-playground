eaaab945aac3dce0d2e7b6dd80cc02e697712d18
jamestheengineer <jamestheengineer@gmail.com>
2022-10-03 21:05:14 -0400

    Start setting up graphQL

---
+++ project-mgmt-app/server/schema/schema.js
@@ -0,0 +1,14 @@
+const { projects, clients } = require('../sampleData.js');
+
+const { GraphQLObjectType } = require('graphql');
+
+// Client Type
+const ClientType = new GraphQLObjectType({
+  name: 'Client',
+  fields: () => ({
+    id: { type: GraphQLID },
+    name: { type: GraphQLString },
+    email: { type: GraphQLString },
+    phone: { type: GraphQLString },
+  }),
+});
