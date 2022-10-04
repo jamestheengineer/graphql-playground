05e09c5f5d44ed18e63395f2e92a96dda88cf9ed
jamestheengineer <jamestheengineer@gmail.com>
2022-10-04 18:25:06 -0400

    Get GraphiQL working

--- project-mgmt-app/server/schema/schema.js
+++ project-mgmt-app/server/schema/schema.js
@@ -1,6 +1,11 @@
 const { projects, clients } = require('../sampleData.js');
 
-const { GraphQLObjectType } = require('graphql');
+const {
+  GraphQLObjectType,
+  GraphQLID,
+  GraphQLString,
+  GraphQLSchema,
+} = require('graphql');
 
 // Client Type
 const ClientType = new GraphQLObjectType({
@@ -11,4 +16,21 @@ const ClientType = new GraphQLObjectType({
     email: { type: GraphQLString },
     phone: { type: GraphQLString },
   }),
+});
+
+const RootQuery = new GraphQLObjectType({
+  name: 'RootQueryType',
+  fields: {
+    client: {
+      type: ClientType,
+      args: { id: { type: GraphQLID } },
+      resolve(parent, args) {
+        return clients.find((client) => client.id === args.id);
+      },
+    },
+  },
+});
+
+module.exports = new GraphQLSchema({
+  query: RootQuery,
 });
