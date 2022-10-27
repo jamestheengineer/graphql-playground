dd194c0f7e383e674ae45e32dea485733686958f
jamestheengineer <jamestheengineer@gmail.com>
2022-10-27 18:36:07 -0400

    Show table of clients

--- project-mgmt-app/client/src/components/Clients.jsx
+++ project-mgmt-app/client/src/components/Clients.jsx
@@ -1,5 +1,5 @@
 import { gql, useQuery } from '@apollo/client';
-
+import ClientRow from './ClientRow';
 const GET_CLIENTS = gql`
   query getClients {
     clients {
@@ -15,5 +15,25 @@ export default function Clients() {
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Something Went Wrong</p>;
 
-  return <>{!loading && !error && <h1>Clients</h1>}</>;
+  return (
+    <>
+      {!loading && !error && (
+        <table className="table table-hover mt-3">
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Phone</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            {data.clients.map((client) => (
+              <ClientRow key={client.id} client={client} />
+            ))}
+          </tbody>
+        </table>
+      )}
+    </>
+  );
 }
